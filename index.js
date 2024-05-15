987d6259338421c1caaba56febc7ccf4ee546068
Sam Hamer <samuel.hamer@outlook.com>
2024-05-14 21:58:57 -0500

    handle validation

--- index.js
+++ index.js
@@ -15,17 +15,6 @@ app.use(
   )
 );
 
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  }
-
-  next(error);
-};
-app.use(errorHandler);
-
 app.get("/info", (request, response, next) => {
   Person.find({})
     .then((persons) => {
@@ -48,21 +37,6 @@ app.get("/api/persons", (request, response, next) => {
 
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
-
-  // if(body.content === undefined) {
-  //   return response.status(400).json({ error: "content missing" });
-  // }
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "content missing",
-    });
-  }
-
-  // if (persons.find((n) => n.name === body.name)) {
-  //   return response.status(400).json({
-  //     error: "person already exists",
-  //   });
-  // }
 
   const person = new Person({
     name: body.name,
@@ -98,18 +72,31 @@ app.delete("/api/persons/:id", (request, response, next) => {
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
     .catch((error) => next(error));
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
--- modules/person.js
+++ modules/person.js
@@ -17,8 +17,21 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
